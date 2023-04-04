ef34f60025a5caf122f6096bb9ec871b06bfadfe
msrisujan <srisujanbannu@gmail.com>
2023-04-04 19:54:19 +0530

    hooks and mapping

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+
 
 function App() {
     return (
         <div>
         <Header />
-        <Note />
+        {notes.map(newnote => (
+            <Note
+            key={newnote.key}
+            title={newnote.title}
+            content={newnote.content}
+            />
+        ))}
         <Footer />
         </div>
     );
