d37acb5541500d5a7d4e43200611c0e78d006aff
msrisujan <srisujanbannu@gmail.com>
2023-04-05 12:59:17 +0530

    react forms and react todolist

---
+++ react-forms/src/components/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function App() {
+  const [name, setName] = React.useState("");
+  const [headingText, setHeading] = React.useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+  function handleClick(event) {
+    setHeading(name);
+
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText} </h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
